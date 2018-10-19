b55dc1d67d716db8836e82702f0edfd06839720f
Carlos Castillo <carloscastillo090916@gmail.com>
2018-10-19 10:26:18 -0500

    Agregando ejercicio clase3

--- Clase3/src/sobrecarga/Principal.java
+++ Clase3/src/sobrecarga/Principal.java
@@ -18,6 +18,7 @@ public class Principal {
         
         Trabajador t = new Trabajador();//Objeto de la clase trabajador
         double sueldo;
+        double semana1, semana2, semana3, semana4;
         double []sueldos = new double[4];//Arreglo que permite guardar los valores.
         
         System.out.printf("Número de nombres del trabajador 1 o 2: ");
@@ -52,12 +53,33 @@ public class Principal {
             t.agregar_sueldo(sueldo);
                 
         }else{
-            for(int x = 0; x<sueldos.length; x++){
+            System.out.println("\nSUELDO MENSUAL");
+            System.out.printf("\n\t1. Forma 1\n\t2. Forma 2\nIngrese una opcion: ");
+            int opcion2 = scanner.nextInt();
+            if(opcion2 == 1){//Envia el arreglo con los sueldos
+                for(int x = 0; x<sueldos.length; x++){
                 System.out.printf("Ingrese el sueldo de la semana %d: ",x + 1);
                 sueldos[x] = scanner.nextDouble();
                
+                }
+                t.agregar_sueldo(sueldos);
+            }else{//Pide los 4 valores
+                System.out.printf("\nIngrese sueldo semana 1: ");
+                semana1 = scanner.nextDouble();
+                
+                System.out.printf("Ingrese sueldo semana 2: ");
+                semana2 = scanner.nextDouble();
+                
+                System.out.printf("Ingrese sueldo semana 3: ");
+                semana3 = scanner.nextDouble();
+                
+                System.out.printf("Ingrese sueldo semana 4: ");
+                semana4 = scanner.nextDouble();
+                
+                t.agregar_sueldo(semana1, semana2, semana3, semana4);
             }
-            t.agregar_sueldo(sueldos);
+            
+            
         }
         
         
